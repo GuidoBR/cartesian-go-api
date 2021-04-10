5dc0e35fe9cd9e66d36f9b46e7c781998220e0e5
Guido Percú <guidopercu@gmail.com>
2021-04-10 11:27:17 -0300

    Fix router

--- main.go
+++ main.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	router := mux.NewRouter()
+	router := NewRouter()
 
 	fmt.Println("Listening to :8000\n")
 	log.Fatal(http.ListenAndServe(":8000", router))

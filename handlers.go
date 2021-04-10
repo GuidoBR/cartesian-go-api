9357a0f6b56a06bbbface7582cbb99896841fba7
Guido Percú <guidopercu@gmail.com>
2021-04-10 13:15:06 -0300

    Refactor handlers

--- handlers.go
+++ handlers.go
@@ -16,19 +16,7 @@ func GetPoints(w http.ResponseWriter, r *http.Request) {
 	// TODO: What if we don't have x, y, d?
 	p1 := Point{x, y}
 
-	f, _ := ioutil.ReadFile("data/points.json")
-	points := Points{}
-	_ = json.Unmarshal([]byte(f), &points)
-
-	pd := Points{}
-	for i := 0; i < len(points); i++ {
-		p2 := Point{points[i].X, points[i].Y}
-		md := ManhattanDistance(p1, p2)
-
-		if md <= d {
-			pd = append(pd, p2)
-		}
-	}
+	pd := GetPointsInsideDistance(p1, d)
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
@@ -37,7 +25,36 @@ func GetPoints(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func ManhattanDistance(p1 Point, p2 Point) int {
+func GetPointsInsideDistance(p1 Point, d int) Points {
+	points := readPointsFromJSON("data/points.json")
+	pd := pointsInsideDistance(points, p1, d)
+
+	return pd
+}
+
+func readPointsFromJSON(p string) Points {
+	f, _ := ioutil.ReadFile(p)
+	points := Points{}
+	_ = json.Unmarshal([]byte(f), &points)
+
+	return points
+}
+
+func pointsInsideDistance(points Points, p1 Point, d int) Points {
+	pd := Points{}
+	for i := 0; i < len(points); i++ {
+		p2 := Point{points[i].X, points[i].Y}
+		md := manhattanDistance(p1, p2)
+
+		if md <= d {
+			pd = append(pd, p2)
+		}
+	}
+
+	return pd
+}
+
+func manhattanDistance(p1 Point, p2 Point) int {
 	d := abs(p1.X-p2.X) + abs(p1.Y-p2.Y)
 	return d
 }
